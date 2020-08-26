a15e5d76c85ab73fe4d5fce38630246257b7594e
CyR1en <cyrien3519@gmail.com>
2020-08-26 14:19:37 -0600

    Update Icon

--- utils.py
+++ utils.py
@@ -5,5 +5,5 @@ import os
 def create_image():
     # Generate an image and draw a pattern
     resource_dir = os.path.join(os.getcwd(), 'resources')
-    image = Image.open(os.path.join(resource_dir, 'icon.png'))
+    image = Image.open(os.path.join(resource_dir, 'image.png'))
     return image
